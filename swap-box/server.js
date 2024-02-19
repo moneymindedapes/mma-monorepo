eff576b4d6b40d3a1998eaeb7fd504db43b58a33
Justin Shaw <mogulx_operates@yahoo.com>
2024-02-19 10:15:06 -0600

    refactor: use port from PORT env var

--- swap-box/server.js
+++ swap-box/server.js
@@ -1,14 +1,20 @@
 import Fastify from 'fastify';
 import health from './routes/health.js';
 
+if (!process.env.PORT) {
+  throw new Error(
+    'Please specify a PORT in the environment. (eg. PORT=3001 npm start)'
+  );
+}
+
 const fastify = new Fastify({ logger: true });
 
-fastify.register(health, { prefix: '/v1' });
+fastify.register(health);
 
 // Start the server
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(process.env.PORT);
     fastify.log.info(`Server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
